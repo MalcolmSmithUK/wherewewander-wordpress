326113d7ac0df4042911237b35b3278325a2dbfc
Malcolm Smith <msmith@msinteractive.co.uk>
2016-01-10 12:16:16 +0000

    [FEATURE] Updated screen reader class

--- comments.php
+++ comments.php
@@ -37,7 +37,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'where-we-wander' ); ?></h2>
+			<h2 class="u-screen-reader"><?php esc_html_e( 'Comment navigation', 'where-we-wander' ); ?></h2>
 			<div class="nav-links">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'where-we-wander' ) ); ?></div>
@@ -58,7 +58,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'where-we-wander' ); ?></h2>
+			<h2 class="u-screen-reader"><?php esc_html_e( 'Comment navigation', 'where-we-wander' ); ?></h2>
 			<div class="nav-links">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'where-we-wander' ) ); ?></div>
